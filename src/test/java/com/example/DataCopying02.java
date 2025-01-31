b2e77c58d5ceed1ede18997164fa2c5eb459a2ca
Ravindra1026 <rathoreravindra1026@gmail.com>
2025-01-31 18:30:39 +0530

    31-Jan-2025

--- src/test/java/com/example/DataCopying02.java
+++ src/test/java/com/example/DataCopying02.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -14,15 +9,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
-public class DataCopying {
-
+public class DataCopying02 {
     EdgeDriver driver;
 
     @BeforeTest
@@ -31,7 +21,6 @@ public class DataCopying {
         edgeOptions.addArguments("--start-maximized");
         driver = new EdgeDriver(edgeOptions);
     }
-
     @Test
     public void test_get_bihar_data() {
         String URL = "https://voters.eci.gov.in/download-eroll?stateCode=S04";
@@ -65,48 +54,40 @@ public class DataCopying {
         lastPage.click();
         WebElement pageCount = driver.findElement(By.xpath("(//span[@class='m-2 control-btn2'])[1]"));
         int pageNumber = Integer.parseInt(pageCount.getText());
-        System.out.println(pageNumber);
+        System.out.println("Total Pages: " + pageNumber);
 
         WebElement firstPage = driver.findElement(By.xpath("//button[normalize-space()='<<']"));
-        firstPage.click();
         WebElement nextPageElement = driver.findElement(By.xpath("//button[normalize-space()='>']"));
 
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Voter Data");
+        // Scrape data from page 1
+        extractTableData();
 
-        int rowCount = 0;
-
-        for (int k = 1; k <= pageNumber; k++) {
-            if (firstPage.isDisplayed()) {
-
-
-                // Get all rows in the table
-                List<WebElement> rows = driver.findElements(By.xpath("//table[@role='table']/tbody/tr"));
-
-                // Loop through each row
-                for (int i = 1; i <= rows.size(); i++) {
-                    // Get all columns in the current row
-                    Row excelRow = sheet.createRow(rowCount++);
-                    List<WebElement> cols = driver.findElements(By.xpath("//table[@role='table']/tbody/tr[" + i + "]/td"));
-
-                    // Loop through each column in the row
-                    for (int j = 1; j <= cols.size(); j++) {
-                        String dynamicXpath = "//table[@role='table']/tbody/tr[" + i + "]/td[" + j + "]";
-                        String data = driver.findElement(By.xpath(dynamicXpath)).getText();
-                        System.out.print(data + "\t");
-                        Cell cell = excelRow.createCell(j - 1);
-                        cell.setCellValue(data);
-                    }
-                    System.out.println();  // Move to the next line after each row
-                }
-                nextPageElement.click();
+        // Now navigate through the rest of the pages (2 to pageNumber)
+        for (int k = 1; k < pageNumber; k++) {
+            if (nextPageElement.isDisplayed()) {
+                nextPageElement.click();  // Go to the next page
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@role='table']/tbody")));  // Wait for the page data to load
+                extractTableData();  // Extract data from the current page
             }
         }
-        try (FileOutputStream fileOut = new FileOutputStream(new File("voter_data.xlsx"))) {
-            workbook.write(fileOut);
-            System.out.println("Data written to Excel file successfully!");
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    private void extractTableData() {
+        // Get all rows in the table
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@role='table']/tbody/tr"));
+
+        // Loop through each row
+        for (int i = 1; i <= rows.size(); i++) {
+            // Get all columns in the current row
+            List<WebElement> cols = driver.findElements(By.xpath("//table[@role='table']/tbody/tr[" + i + "]/td"));
+
+            // Loop through each column in the row
+            for (int j = 1; j <= cols.size(); j++) {
+                String dynamicXpath = "//table[@role='table']/tbody/tr[" + i + "]/td[" + j + "]";
+                String data = driver.findElement(By.xpath(dynamicXpath)).getText();
+                System.out.print(data + "\t");
+            }
+            System.out.println();  // Move to the next line after each row
         }
     }
 
@@ -114,4 +95,5 @@ public class DataCopying {
     public void closeBrowser() {
         driver.quit();
     }
+
 }
